3031a87c902a3fbe498c0ca6552c824d66685240
Shakira <shakira.r.salazar@gmail.com>
2021-08-19 12:35:28 +0100

    step 11 actually

--- directory.rb
+++ directory.rb
@@ -93,6 +93,15 @@ def show_students
 	print_footer
 end
 
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+end
 def process(selection)
 	case selection
 		when "1"
